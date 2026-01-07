40ad1b416c6d2c047a6eacd1303f4a9524cd6c57
Weeb Labs <troy2062@gmail.com>
2026-01-07 01:50:55 +0000

    Initial Commit

---
+++ DSPi Console/DSPi_ConsoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSPi_ConsoleApp.swift
+//  DSPi Console
+//
+//  Created by Troy Dunn-Higgins on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct DSPi_ConsoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
